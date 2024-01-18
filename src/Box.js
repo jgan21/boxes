0c479d053cea30d5943efb05abdc882b8b19e94c
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-18 16:35:13 -0600

    added docstring to box

--- src/Box.js
+++ src/Box.js
@@ -1,6 +1,18 @@
 import React from "react";
 
-/**  */
+/** Display box with it's characteristics.
+ *
+ * Props:
+ * - id
+ * - width
+ * - height
+ * - backgroundColor
+ * - removeBox (function)
+ *
+ * State: None
+ *
+ * BoxList -> Box
+ */
 function Box({ id, width, height, backgroundColor, removeBox }) {
   const boxStyle = {
     'width': `${width}px`,
