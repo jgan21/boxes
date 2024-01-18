0710689f9e1247f9e872e59ab378f3285d289c63
Veronica Ni <vni1294@gmail.com>
2024-01-18 17:12:53 -0500

    Added beginning of NewBoxForm

---
+++ src/NewBoxForm.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function NewBoxForm({ addBox }) {
+  const initialState = {
+    width: "",
+    height: "",
+    backgroundColor: "",
+  };
+  const [formData, setFormData] = useState(initialState);
+
+  //TODO: jia!
+}
